0ac98a7f200a879a66c64c06fba6fb4de7423483
Alina Borisova <b-alina1999.borisova@yandex.ru>
2022-05-14 20:49:31 +0300

    First

--- script/index.js
+++ script/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const heading = document.querySelectorAll('h1');
+console.log(heading);
+const modalForm = document.querySelectorAll('form');
+console.log(modalForm);
+const modalCheckbox = document.querySelectorAll('.modal__checkbox');
+console.log(modalCheckbox);
+const nearCheckbox = document.querySelectorAll('.modal__checkbox-wrapper');
+console.log(nearCheckbox);
+
