fb4680dded17854d07d88ea0c94a46e29a090f3d
Alina Borisova <b-alina1999.borisova@yandex.ru>
2022-07-04 12:50:18 +0300

    Удаление строк

--- script/Lesson_5.js
+++ script/Lesson_5.js
@@ -6,15 +6,28 @@ btnAdd.addEventListener('click', () => {
 });
 
 const form = document.querySelector('.overlay');
-form.addEventListener('click', event => {
-  event.stopPropagation();
-});
+ form.addEventListener('click', e => {
+  const target = e.target;
+   console.log(target)
+   if (target === form ||
+        target.closest('.overlay__modal modal')) {
+         form.classList.remove('active');
+   };
+ });
 
-form.addEventListener('click', () => {
-  form.classList.remove('active')
-});
-
-const btnClose = document.querySelector('.modal__close');
+ const btnClose = document.querySelector('.modal__close');
   btnClose.addEventListener('click', () => {
     form.classList.remove('active');
   });
+
+const btnDel = document.querySelectorAll('.table__btn_del');
+  for (let i = 0; i < btnDel.length; i++) {
+    btnDel[i].addEventListener('click', e => {
+    const target = e.target;
+      if (target.closest('.table__body')) {
+        target.closest('tr').remove();
+        base.splice([i], 1);
+        console.log(base);
+      };
+    });
+  };
